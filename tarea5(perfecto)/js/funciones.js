1b078ac80ec67b8256e83360514507197d0bd63d
BerniNehuen <128099444+BerniNehuen@users.noreply.github.com>
2023-03-16 19:31:07 -0300

    Add files via upload

---
+++ tarea5(perfecto)/js/funciones.js
@@ -0,0 +1,20 @@
+function EsPerfecto(n1){
+
+let x, total=0 ,contador=0;
+x=n1 / 2;
+
+	for(let i=1 ; i<=x ; i++){
+
+		if(n1%i==0){
+
+		total+=n1;
+
+		}
+
+	}
+
+	if (total==n1){
+		document.write("Es Perfecto");
+	}
+
+}
