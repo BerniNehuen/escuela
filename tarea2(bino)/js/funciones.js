5419a9d2d10a20d07a46a5b3dd87faec4489646b
BerniNehuen <128099444+BerniNehuen@users.noreply.github.com>
2023-03-16 19:19:56 -0300

    Add files via upload

---
+++ tarea2(bino)/js/funciones.js
@@ -0,0 +1,55 @@
+function ContarMultiplosde2(s,cantidad){
+
+
+let i,multiplosde2=0;
+	for(i=0;i<=cantidad;i++){
+
+		if(s[i]=="2"){
+			multiplosde2++;
+		}
+	}
+
+return multiplosde2;
+}
+
+function ContarMultiplosde3(s,cantidad){
+
+
+let i,multiplosde2=0;
+	for(i=0;i<=cantidad;i++){
+
+		if(s[i]=="3"){
+			multiplosde2++;
+		}
+	}
+
+return multiplosde2;
+}
+
+function ContarMultiplosde4(s,cantidad){
+
+
+let i,multiplosde2=0;
+	for(i=0;i<=cantidad;i++){
+
+		if(s[i]=="4"){
+			multiplosde2++;
+		}
+	}
+
+return multiplosde2;
+}
+
+function ContarMultiplosde5(s,cantidad){
+
+
+let i,multiplosde2=0;
+	for(i=0;i<=cantidad;i++){
+
+		if(s[i]=="5"){
+			multiplosde2++;
+		}
+}
+
+return multiplosde2;
+}
