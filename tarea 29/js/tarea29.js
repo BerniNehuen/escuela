e3cee6f96a9455183fa0b54db23003e170d9d82b
BerniNehuen <128099444+BerniNehuen@users.noreply.github.com>
2023-06-28 20:44:51 -0300

    Add files via upload

---
+++ tarea 29/js/tarea29.js
@@ -0,0 +1,13 @@
+class CRectangulo{
+	constructor(longitud,altura){
+		this.longitud=longitud;
+		this.altura=altura;
+	}
+
+    CalcularArea(){
+    	let calcular = this.longitud * this.altura;
+    	return calcular;
+    }
+
+
+}
